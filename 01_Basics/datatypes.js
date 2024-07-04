abea34f43761f610e4940d7834dab032fd35b145
Saniya Mulla <saniyamulla2105@gmail.com>
2024-07-04 19:13:57 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+//alert("hello");
+//alert(3+3);//if we run this on node it will give error so we are running this on browser in console
+console.log(3+3);console.log("saniya")//semicolon is mandatory otherwise it will give error but this is not the correct way of writing code
+let name="saniya";//string data type
+let age=20;//number
+let isLoggedIn=false;
+let state;
+//datatypes:
+//1)Number =>2 to power 53
+//bigint
+//string=>""
+//boolean=>true or false
+//null=>standalone value
+//undefined=>variable is declared but if there is not any value that will be undefined
+//symbol=>unique
+//object
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
