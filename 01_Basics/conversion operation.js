a6e4ff94830b078b82075bdee0ab109cb0b9a3a2
Saniya Mulla <saniyamulla2105@gmail.com>
2024-07-05 20:16:37 +0000

    conversion

--- 01_Basics/conversion operation.js
+++ 01_Basics/conversion operation.js
@@ -4,3 +4,20 @@ console.log(typeof score);
 console.log(typeof (score));
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
+let str1="hello";
+let str2="saniya";
+let str3=str1+" "+str2;
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log((3+4)*(5)%(3));
+console.log(+true);
+console.log(+"");
+
+let gameCounter=100;
+++gameCounter;//prefix and postfix in javascript
+console.log(gameCounter);
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+//refer MDN documentation
