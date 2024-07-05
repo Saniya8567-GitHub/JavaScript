9ffdc4e3e2b571af1a95b51f20d97a602bd8cafc
Saniya Mulla <saniyamulla2105@gmail.com>
2024-07-05 20:18:57 +0000

    comparison

---
+++ 01_Basics/comparisons.js
@@ -0,0 +1,14 @@
+//comparison
+//easy comparisons
+//console,log(2>1);
+//console.log(2>=1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+console.log("2">1);//output true
+console.log("02">1);//output true
+ console.log(null>0);
+ console.log(null==0);
+ console.log(null>=0);
+ console.log(undefined==0);
+ 
