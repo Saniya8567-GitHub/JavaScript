9402fd2683c649bda7092f15a987544d325c5134
Saniya Mulla <saniyamulla2105@gmail.com>
2025-09-16 19:26:40 +0000

    datatype confusion

---
+++ 01_Basics/datatypes confusion.js
@@ -0,0 +1,9 @@
+let score=33
+console.log(typeof (score))
+let valueIsNumber=Number(score)
+console.log(typeof(score));
+
+let isLoggedIn=1
+let booleanIsLogged=Boolean(isLoggedIn)
+console.log( booleanIsLogged);
+console.log(typeof(isLoggedIn));
