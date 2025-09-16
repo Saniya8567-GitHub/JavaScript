1995d8cbf9a9c5addce22f8314fc0f381a4fa821
Saniya Mulla <saniyamulla2105@gmail.com>
2025-09-16 18:46:05 +0000

    changes to be committed done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,4 +2,10 @@ const accountId=144553
 let accountEmail = "saniyamulla@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-accountId=2
+let accountState;
+//accountId=2
+accountEmail="Saniya@321"
+accountPassword="123456"
+accountCity="Banglore"
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity,accountState]);
