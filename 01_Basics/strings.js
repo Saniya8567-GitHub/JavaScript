6b4c57bd49289ce4cdd5b058fc5e540f0e0811ee
Saniya Mulla <saniyamulla2105@gmail.com>
2025-10-03 17:13:38 +0000

    strings in js

---
+++ 01_Basics/strings.js
@@ -0,0 +1,11 @@
+const name="Saniya";
+const repocount=50;
+//console.log(name + repocount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName=new String('saniyaaaa');
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
