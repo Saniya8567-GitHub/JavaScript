b3e22cfab0e9982e27c719aa14dc204ed592b557
Saniya Mulla <saniyamulla2105@gmail.com>
2025-10-03 15:44:51 +0000

    " learn Stack and Heap memory"

---
+++ 01_Basics/memory.js
@@ -0,0 +1,14 @@
+let myYoutubename = "saniyamulladorcom"
+let anothername = myYoutubename
+anothername ="ChaiaurCode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+let userTwo= userOne
+userTwo.email="hitesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
