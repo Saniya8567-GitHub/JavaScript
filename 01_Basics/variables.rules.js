70d89e2e3951924aa1ee44b76347e2831cc30760
Saniya Mulla <saniyamulla2105@gmail.com>
2025-10-19 16:56:11 +0000

    variables.rules

---
+++ 01_Basics/variables.rules.js
@@ -0,0 +1,4 @@
+//Variables are cases sensitive: "a " and "A" are different variables.
+//only letters,digits,$ and _ are valid characters for variable names.
+//Variables cannot start with a digit.
+//reserved words cannot be used as variable names.
