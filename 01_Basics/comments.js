dab9b0b74ebcad9773de348d797a33bd54750bfb
Saniya Mulla <saniyamulla2105@gmail.com>
2025-10-21 18:29:33 +0000

    comments

---
+++ 01_Basics/comments.js
@@ -0,0 +1,12 @@
+//this code prints hello world -single line comment
+/* this code that prints 
+hello world but on our console window*---Multiline comment*/
+console.log("Hello world");
+//Operations in js
+let a=5;
+let b=6;
+console.log("a+b=", a+b);
+console.log("a-b=", a-b);
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);
